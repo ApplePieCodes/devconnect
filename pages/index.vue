63a698605a8d94a618a46242a35d5c01164ec4ef
ApplePieCodes <127224911+ApplePieCodes@users.noreply.github.com>
2025-07-21 22:02:46 -0400

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -17,40 +17,40 @@ onMounted(async () => {
 <template>
   <div class="bg-purple-950 min-h-screen">
     <Navbar/>
-    <div class="mt-30 flex flex-col">
+    <div class="mt-5">
       <div class="text-center">
-        <h1 class="text-8xl font-mono font-bold text-purple-500 drop-shadow-2xl">
+        <h1 class="text-6xl lg:text-8xl font-mono font-bold text-purple-500 drop-shadow-2xl">
           DevConnect
         </h1>
-        <h2 class="font-mono text-purple-400 text-3xl">
+        <h2 class="text-xl lg:text-3xl font-mono text-purple-400">
           The Social Media <i>for</i> Developers
         </h2>
-        <p class="mt-2 text-xl text-purple-300 font-mono mx-auto max-w-xl">
+        <p class="text-lg lg:text-2xl lg:mx-10 font-mono text-purple-300 mt-2 mx-2">
           DevConnect is a social media platform centered around developers, software development, hardware development, and all things tech.
         </p>
 
-        <section class="mt-10 text-left">
-          <div class="max-w-6xl mx-auto grid gap-12 grid-cols-3">
+        <section class="mt-5 mx-10 md:mx-20">
+          <div class="grid gap-5 grid-cols-1 lg:grid-cols-3">
             <div class="bg-purple-800 px-5 py-5 rounded-2xl hover:drop-shadow-2xl transition-all duration-200 ease-in-out">
-              <h3 class="text-3xl font-bold font-mono mb-1 text-purple-100">Dev-Focused</h3>
+              <h3 class="text-3xl font-bold font-mono text-purple-100">Dev-Focused</h3>
               <p class="text-purple-100 font-mono">Built specifically for developers with tech related tags for posts.</p>
             </div>
             <div class="bg-purple-800 px-5 py-5 rounded-2xl hover:drop-shadow-2xl transition-all duration-200 ease-in-out">
-              <h3 class="text-3xl font-bold font-mono mb-1 text-purple-100">Clean & Minimal</h3>
+              <h3 class="text-3xl font-bold font-mono text-purple-100">Clean & Minimal</h3>
               <p class="text-purple-100 font-mono">Unintrusive ads, clean design and open-source. <span class="font-bold">You</span> can contribute!</p>
             </div>
             <div class="bg-purple-800 px-5 py-5 rounded-2xl hover:drop-shadow-2xl transition-all duration-200 ease-in-out">
-              <h3 class="text-3xl font-bold font-mono mb-1 text-purple-100">Find Work</h3>
+              <h3 class="text-3xl font-bold font-mono text-purple-100">Find Work</h3>
               <p class="text-purple-100 font-mono">Advertise freelance programming or look for teams.</p>
             </div>
           </div>
         </section>
 
-        <section class="pt-8">
-          <h2 class="text-4xl font-bold font-mono mb-4 text-purple-100">Join the community</h2>
-          <p class="text-lg mb-8 text-purple-100 font-mono">Start sharing your builds, ideas, and tools with other developers.</p>
-          <NuxtLink to="/signup" v-if="signedIn" class="bg-purple-500 text-purple-100 px-6 py-3 rounded-2xl font-bold font-mono hover:bg-purple-600 transition-all duration-300 hover:drop-shadow-2xl">Sign Up</NuxtLink>
-          <NuxtLink to="/dashboard" v-else class="bg-purple-500 text-purple-100 px-6 py-3 rounded-2xl font-bold font-mono hover:bg-purple-600 transition-all duration-300 hover:drop-shadow-2xl">Go to Dashboard</NuxtLink>
+        <section class="pt-5 pb-10">
+          <h2 class="text-3xl xl:text-6xl font-bold font-mono mb-2 text-purple-100">Join the community</h2>
+          <p class="text-lg xl:text-2xl mb-4 mx-5 text-purple-100 font-mono">Start sharing your builds, ideas, and tools with other developers.</p>
+          <NuxtLink to="/signup" v-if="signedIn" class="xl:text-xl bg-purple-500 inline-block text-purple-100 px-6 py-3 rounded-2xl font-bold font-mono hover:bg-purple-600 transition-all duration-300 hover:drop-shadow-2xl">Sign Up</NuxtLink>
+          <NuxtLink to="/dashboard" v-else class=" xl:text-xl bg-purple-500 inline-block text-purple-100 px-6 py-3 rounded-2xl font-bold font-mono hover:bg-purple-600 transition-all duration-300 hover:drop-shadow-2xl">Go to Dashboard</NuxtLink>
         </section>
       </div>
     </div>
--- server/components/Navbar.vue
+++ server/components/Navbar.vue
@@ -16,12 +16,11 @@
 
 <template>
   <div class="pb-14">
-    <nav class="z-50 fixed top-0 left-0 right-40 bg-background/80 backdrop-blur-sm border-b min-w-screen">
-      <div class="container mx-auto">
+    <nav class="z-100 fixed top-0 left-0 right-40 bg-background/70 backdrop-blur-sm border-b min-w-screen">
+      <div class="mx-auto">
         <div class="flex items-center justify-between h-16">
           <div class="flex items-center justify-between">
-            <NuxtLink href="/" class="text-4xl ml-5 font-mono font-bold transition-colors text-purple-100 hover:text-purple-300 transition-all duration-300 hover:drop-shadow-2xl">DevConnect</NuxtLink>
-            <NuxtLink href="/dashboard" class="text-xl mt-1 ml-5 font-mono transition-colors text-purple-100 hover:text-purple-300 transition-all duration-300 hover:drop-shadow-2xl">Dashboard</NuxtLink>
+            <NuxtLink href="/dashboard" class="text-4xl ml-5 font-mono font-bold transition-colors text-purple-100 hover:text-purple-300 transition-all duration-300 hover:drop-shadow-2xl">DevConnect</NuxtLink>
           </div>
           <div class="mr-5">
             <NuxtLink to="/signup" class="bg-purple-500 text-purple-100 px-4 py-3 rounded-2xl font-bold font-mono hover:bg-purple-600 transition-all duration-300 hover:drop-shadow-2xl" v-if="!loggedIn">Sign Up</NuxtLink>
